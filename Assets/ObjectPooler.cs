16f1ab85f2a520a55706728f13b60384c0029daa
lqsisto <36103237+lqsisto@users.noreply.github.com>
2019-07-25 12:07:59 +0300

    Object Pooler

---
+++ Assets/ObjectPooler.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class ObjectPooler : MonoBehaviour
+{
+    [SerializeField] private static GameObject objectHolder;
+    
+    //prefab array
+    [SerializeField] private Transform[] powerUpPrefabs;
+    
+    //object array
+    [SerializeField] private PowerUp [] powerUpObjects;
+    
+    [SerializeField] private int poolSize;
+    
+    //Dictionary where both the key and the queue type are the same powerup prefab
+    private static Dictionary<Transform, Queue<Transform>> PoolDict = new Dictionary<Transform, Queue<Transform>>();
+    
+    private static ObjectPooler instance;
+    
+    private PowerUp powerUp;
+
+    public bool poolInitialized = false;
+    
+    private void Awake ()
+    {
+        if (instance == false)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy (this);
+        }
+        
+        objectHolder = GameObject.Find ("ObjectHolder");
+        for(int i = 0; i < powerUpObjects.Length; i++)
+        {
+            powerUp = new PowerUp ();
+            PoolDict.Add (powerUpPrefabs[i], powerUp.CreatePool (poolSize, powerUpPrefabs[i]));
+        }
+
+        print ($"Pools created: {PoolDict.Keys.Count}");
+        poolInitialized = true;
+    }
+
+    public Queue<Transform> TryGetPowerUp ()
+    {
+        var queueToReturn = Randomize (powerUpPrefabs);
+        
+        if (queueToReturn.Count == 0)
+        {
+            print ("QUEUE IS NULL, FETCHING OTHER ONE");
+            TryGetPowerUp ();
+        }
+        print ("RETURNING QUEUE!");
+        return queueToReturn;
+    }
+
+    public static void ReturnPowerUp (Queue<Transform> powerUpQueue, Transform powerUpObject)
+    {
+        powerUpObject.parent = objectHolder.transform;
+        powerUpQueue.Enqueue (powerUpObject);
+    }
+
+    private Queue<Transform> Randomize (IReadOnlyList<Transform> objects)
+    {
+        var random = Random.Range (0, objects.Count);
+        return PoolDict[objects[random]];
+    }
+
+    [System.Serializable]
+    public class PowerUp
+    {
+        public string name;
+        public Queue<Transform> pool;
+        
+        public Queue<Transform> CreatePool (int poolSize, Transform poolableObject)
+        {
+            pool = new Queue<Transform> ();
+            for (var i = 0; i < poolSize; i++)
+            {
+                var pu = Instantiate (poolableObject,objectHolder.transform);
+                pu.transform.position = Vector3.zero;
+                pu.gameObject.SetActive (false);
+                pool.Enqueue (pu);
+            }
+            return pool;
+        }
+    }
+}
---
+++ Assets/Spawner.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using TMPro;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public ObjectPooler pooler;
+
+    private Queue<Transform> powerUpQueue;
+
+    private Transform powerUp;
+    // Start is called before the first frame update
+   private  void Start()
+   {
+       pooler = FindObjectOfType<ObjectPooler> ();
+        StartCoroutine (SetPowerUpActive ());
+    }
+
+    private IEnumerator SetPowerUpActive ()
+    {
+
+        while (!pooler.poolInitialized)
+        {
+            print ("POOLER NOT INITIALIZED");
+            yield return null;
+        }
+        print ("POOLER INITIALIZED");
+        
+        powerUpQueue = pooler.TryGetPowerUp ();
+        powerUp = powerUpQueue.Dequeue ();
+
+        var transform1 = powerUp.transform;
+        
+        transform1.parent = gameObject.transform;
+        transform1.localPosition = Vector3.zero;
+        powerUp.gameObject.SetActive (true);
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown (KeyCode.Space))
+        {
+            powerUp.gameObject.SetActive (false);
+            powerUp.transform.parent = null;
+            ObjectPooler.ReturnPowerUp (powerUpQueue, powerUp);
+        }
+    }
+}
